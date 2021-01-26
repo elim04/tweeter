ba343914d014cd22b159648ffd135728cba53d82
Emily Lim <emily.elim04@gmail.com>
2021-01-26 20:00:35 +0000

    create counter for character input in new-tweet section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
+  const tweet = $('textarea');
 
+  tweet.on('input', function(event) {
+
+  let characterCount = (140 - $(this).val().length);
+
+  let counter = $(this).closest('form').children('.buttonContainer').children('.counter');
+
+  if (characterCount < 0) {
+    counter.addClass('overCharLimit');
+  } else {
+    counter.removeClass('overCharLimit')
+  }
   
+  counter.html(characterCount);
+  
+
+  });
+
+
+
+
 });
