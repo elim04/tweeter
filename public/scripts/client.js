0df314560fbd00cc08c93799ee0564354bbdf4d1
Emily Lim <emily.elim04@gmail.com>
2021-01-27 19:50:11 +0000

    create ajax post request to server for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,40 @@
+const createTweetElement = function(tweet) {
+
+  const htmlTweet = `
+    <article class="tweet">
+    <header>
+      <div class="leftSide">
+        <img src='${tweet.user.avatars}'>
+        <p id="firstName">${tweet.user.name}</p>
+      </div>
+      <div class="rightSide" >
+        <p>${tweet.user.handle}</p>
+      </div>
+    </header>
+    <p class="tweetContent">${tweet.content.text}</p>
+    <footer>
+      <div class="contentFooter">
+        <p>${tweet.created_at}</p>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </div>
+    </footer>
+    </article>
+    `
+  return htmlTweet;
+};
+
+const renderTweets = function(tweetData){
+  //create the html element for tweet
+  for (let tweet of tweetData) {
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
+  }
+
+};
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -31,47 +68,26 @@ $(document).ready(function() {
       "created_at": 1611619414698
     }
   ]
-    
-  const createTweetElement = function(tweet) {
-
-    const htmlTweet = `
-    <article class="tweet">
-    <header>
-      <div class="leftSide">
-        <img src='${tweet.user.avatars}'>
-        <p id="firstName">${tweet.user.name}</p>
-      </div>
-      <div class="rightSide" >
-        <p>${tweet.user.handle}</p>
-      </div>
-    </header>
-    <p class="tweetContent">${tweet.content.text}</p>
-    <footer>
-      <div class="contentFooter">
-        <p>${tweet.created_at}</p>
-        <div class="icons">
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </div>
-      </div>
-    </footer>
-    </article>
-    `
-    return htmlTweet;
-  };
-
-  const renderTweets = function(tweetData){
-    //create the html element for tweet
-    for (let tweet of tweetData) {
-      let $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
-    }
-
-  };
 
   renderTweets(tweetData);
 
-  // console.log($tweet);
-  // $('#tweets-container').append($tweet);
+
+  $('#tweet-form').on('submit', function(event) {
+
+    event.preventDefault();
+
+    const formContent = $(this).serialize();
+    console.log(formContent)
+    
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'POST',
+      data: formContent
+    })
+    .done((result) => console.log(result))
+    .fail(() => console.log('Error!'))
+    .always(() => console.log('Request completed'))
+
+  })
+
 });
